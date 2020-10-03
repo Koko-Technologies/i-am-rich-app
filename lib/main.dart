4c71333c6235516d3914f6ced5859f71ca4005ac
iqballiii <meet.iqbal11@gmail.com>
2020-10-03 19:46:51 +0530

    animated text kit package added

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 void main() {
   runApp(IamRich());
 }
 
 class IamRich extends StatelessWidget {
+  ScrollController _scrollController;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.blueGrey[500],
+        backgroundColor: Colors.blueGrey,
         appBar: AppBar(
-          title: Text('I am rich!'),
-          backgroundColor: Colors.blueGrey[900],
+          backgroundColor: Colors.blueGrey.shade800,
+          title: Text(
+            'I Am Rich!',
+            style: TextStyle(
+              fontSize: 20.0,
+            ),
+          ),
         ),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Image(
-              image: AssetImage('images/diamond.png'),
-              fit: BoxFit.contain,
+            Container(
+              alignment: Alignment.center,
+              child: Image(
+                image: AssetImage('images/diamond.png'),
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
             ),
             Container(
-                color: Colors.blue,
-                child: Text('\"Shine Bright Like a Diamond\" ~ Rihanna')),
+              padding: EdgeInsets.symmetric(horizontal: 15.0),
+              height: 100.0,
+              color: Colors.yellow,
+              width: double.infinity,
+              alignment: Alignment.center,
+              child: FadeAnimatedTextKit(
+                onTap: () {
+                  print("Tap Event");
+                },
+                isRepeatingAnimation: true,
+                repeatForever: true,
+                text: [
+                  'Shine bright like a diamond',
+                  'Shine bright like a diamond',
+                  'Find light in the beautiful sea, I choose to be happy',
+                  'You and I, you and I , we\'re like diamonds in the sky',
+                  'You\'re a shooting star I see, a vision of ecstasy',
+                  'When you hold me, I\'m alive',
+                  'We\'re like diamonds in the sky',
+                ],
+                // alignment: Alignment(1.0, 0.5),
+                textStyle: TextStyle(
+                  fontSize: 25.0,
+                ),
+              ),
+            ),
           ],
         ),
       ),
